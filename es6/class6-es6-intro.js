6e0bb623b45181b96056e21a97c013cc7a1be55e
kazi mohammed foysal <foysalkazimd01@gmail.com>
2024-01-04 20:46:28 +0600

    udpated

---
+++ es6/class6-es6-intro.js
@@ -0,0 +1,14 @@
+// let a = 100;
+// let b = 200;
+
+// function addAB(a,b){
+// 	return a + b;
+// }
+
+// console.log(addAB(a,b));
+
+let a = 100;
+let b = 300;
+let sum = (a,b) => a + b;
+
+console.log(sum(a,b));
