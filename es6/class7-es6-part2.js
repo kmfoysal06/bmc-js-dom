4497314321582e32fff852cb94bb645f4b0eb954
kazi mohammed foysal <foysalkazimd01@gmail.com>
2024-01-11 16:35:43 +0600

    successfully created two new files and done that files with coding of last class

--- es6/class7-es6-part2.js
+++ es6/class7-es6-part2.js
@@ -1,41 +1,41 @@
-const name = 'foysal';
-const age  = 12;
-const normalConcat = 'HI! i am ' +name+ ' and i am ' +age+ ' year old.';
-console.log(normalConcat);
+// const name = 'foysal';
+// const age  = 12;
+// const normalConcat = 'HI! i am ' +name+ ' and i am ' +age+ ' year old.';
+// console.log(normalConcat);
 
-const templateLiteral = `Hi am ${name} and i am ${age} year old.`;
-console.log(templateLiteral)
+// const templateLiteral = `Hi am ${name} and i am ${age} year old.`;
+// console.log(templateLiteral)
 
-const friends = ['aziz','asif','asraf'];
-const azizFriends = ['asraf','atif'];
-const asifFriends = ['anas','abrar'];
-const ashrafFriends = ['abida','anisha'];
+// const friends = ['aziz','asif','asraf'];
+// const azizFriends = ['asraf','atif'];
+// const asifFriends = ['anas','abrar'];
+// const ashrafFriends = ['abida','anisha'];
 
-const mutualFriends = [...azizFriends,...asifFriends,...asifFriends];
-console.log(mutualFriends);
+// const mutualFriends = [...azizFriends,...asifFriends,...asifFriends];
+// console.log(mutualFriends);
 
-const foysal = {name:'foysal',age:'NaN',aim:'web developer',dream:'be great',nightmare:'error'}
+// const foysal = {name:'foysal',age:'NaN',aim:'web developer',dream:'be great',nightmare:'error'}
 
-const {name,age,aim} = foysal;
-console.log(name)
-console.log(age)
-console.log(aim)
+// const {name,age,aim} = foysal;
+// console.log(name)
+// console.log(age)
+// console.log(aim)
 
-class Parent{
-	constructor(){
-		this.instructor = 'ad gupta sir';
-	}
-}
+// class Parent{
+// 	constructor(){
+// 		this.instructor = 'ad gupta sir';
+// 	}
+// }
 
-class User extends Parent{
-	constructor(name,age){
-		super();
-		this.name = name;
-		this.age = age;
-	}
-}
+// class User extends Parent{
+// 	constructor(name,age){
+// 		super();
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// }
 
-const foysal = new User('kazi foysal',1020);
+// const foysal = new User('kazi foysal',1020);
 
-const simpleBio = `Hi , i am ${foysal.name} i am nan ${foysal.age} old and my instructor is ${foysal.instructor}`;
-console.log(simpleBio)
+// const simpleBio = `Hi , i am ${foysal.name} i am nan ${foysal.age} old and my instructor is ${foysal.instructor}`;
+// console.log(simpleBio)
